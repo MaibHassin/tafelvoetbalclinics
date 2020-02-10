4268ffb0163848f02881db0cdb5b38fbff0a8d1d
Maib Hassin Syed <maibhassin@gmail.com>
2020-02-10 12:24:49 +0000

    Added typewriter feature
    - in over mij section article header (introduction) added typewriter
    feature
    - introduction text changes from qaiser syed to his professions and
    titles

--- js/script.js
+++ js/script.js
@@ -68,4 +68,68 @@ $('a[href*="#"]')
   // Copyright year
   var date = new Date();
   document.getElementById('copyright-year').innerHTML = date.getFullYear();
+
+
+  // Typewriter text for over mij section article heading
+
+  const TypeWriter = function(textElement, words, waitTime = 3000) {
+    this.textElement = textElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.waitTime = parseInt(waitTime, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  TypeWriter.prototype.type = function() {
+    // current index of word
+    const current = this.wordIndex % this.words.length;
+    // get full text of current word
+    const fullTxt = this.words[current];
+
+    // check if deleting
+    if(this.isDeleting) {
+      // remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // insert txt into element
+    this.textElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+    // initial type speed
+    let typeSpeed = 100;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // if word is complete 
+    if(!this.isDeleting && this.txt === fullTxt) {
+      //this will make a pause at end
+      typeSpeed = this.waitTime;
+      // set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.wordIndex++;
+      // pause before start typing
+      typeSpeed = 1000;
+    }
+
+    setTimeout(() => this.type(), typeSpeed)
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init() {
+    const textElement = document.querySelector('.ik-ben-type');
+    const words = JSON.parse(textElement.getAttribute('data-words'));
+    const wait = textElement.getAttribute('data-wait');
+    //init typewriter
+    new TypeWriter(textElement, words, wait);
+  }
   
